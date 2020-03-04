babed2e7648f2d9089afd404d1cb77b64cbef415
Naresh Battul <battul.naresh2013@gmail.com>
2020-03-04 08:09:29 +0100

    added few more city names in cityList.js

--- src/shared/cityList.js
+++ src/shared/cityList.js
@@ -10,6 +10,42 @@ export const cities = [
     },
   },
   {
+    id: 1275339,
+    name: 'Mumbai',
+    country: 'IN',
+    coord: {
+      lon: 72.847939,
+      lat: 19.01441,
+    },
+  },
+  {
+    id: 1277333,
+    name: 'Bangalore',
+    country: 'IN',
+    coord: {
+      lon: 77.603287,
+      lat: 12.97623,
+    },
+  },
+  {
+    id: 1264527,
+    name: 'Chennai',
+    country: 'IN',
+    coord: {
+      lon: 80.278473,
+      lat: 13.08784,
+    },
+  },
+  {
+    id: 1275004,
+    name: 'Kolkata',
+    country: 'IN',
+    coord: {
+      lon: 88.36972,
+      lat: 22.569719,
+    },
+  },
+  {
     id: 1256436,
     name: 'Solapur',
     country: 'IN',
@@ -88,6 +124,78 @@ export const cities = [
     coord: {
       lon: 5.98333,
       lat: 52.3475,
+    },
+  },
+  {
+    id: 2756100,
+    name: 'Engeland',
+    country: 'NL',
+    coord: {
+      lon: 6.36944,
+      lat: 52.767502,
+    },
+  },
+  {
+    id: 2756865,
+    name: 'Dieren',
+    country: 'NL',
+    coord: {
+      lon: 6.1,
+      lat: 52.052502,
+    },
+  },
+  {
+    id: 2746609,
+    name: 'Strijen',
+    country: 'NL',
+    coord: {
+      lon: 4.55083,
+      lat: 51.745209,
+    },
+  },
+  {
+    id: 2747891,
+    name: 'Rotterdam',
+    country: 'NL',
+    coord: {
+      lon: 4.47917,
+      lat: 51.922501,
+    },
+  },
+  {
+    id: 2745912,
+    name: 'Utrecht',
+    country: 'NL',
+    coord: {
+      lon: 5.12222,
+      lat: 52.090832,
+    },
+  },
+  {
+    id: 2756253,
+    name: 'Eindhoven',
+    country: 'NL',
+    coord: {
+      lon: 5.47778,
+      lat: 51.44083,
+    },
+  },
+  {
+    id: 2746301,
+    name: 'Tilburg',
+    country: 'NL',
+    coord: {
+      lon: 5.0913,
+      lat: 51.555511,
+    },
+  },
+  {
+    id: 2755251,
+    name: 'Groningen',
+    country: 'NL',
+    coord: {
+      lon: 6.56667,
+      lat: 53.21917,
     },
   },
 ];
